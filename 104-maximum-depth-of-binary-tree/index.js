af055ed3e3448815d2f8eea72532da0a8b7c6f9e
Ahmad Muslih Zain <ahmad.muslih22@gmail.com>
2023-10-17 12:42:07 +0300

    Add 104

--- 104-maximum-depth-of-binary-tree/index.js
+++ 104-maximum-depth-of-binary-tree/index.js
@@ -1,25 +1,35 @@
 const TreeNode = require("../TreeNode");
 
-const node7 = new TreeNode(7);
-const node15 = new TreeNode(15);
-const node20 = new TreeNode(20, node15, node7);
-const node9 = new TreeNode(9)
-const node3 = new TreeNode(3, node9, node20);
+// const node7 = new TreeNode(7);
+// const node15 = new TreeNode(15);
+// const node20 = new TreeNode(20, node15, node7);
+// const node9 = new TreeNode(9);
+// const node3 = new TreeNode(3, node9, node20);
 
+const node5 = new TreeNode(5);
+const node4 = new TreeNode(4);
+const node3 = new TreeNode(3);
+const node2 = new TreeNode(2, node4, node5);
+const node1 = new TreeNode(1, node2, node3);
+const root = node1;
 
+var maxDepth = function (root) {
+  if (!root) return 0;
+  const queue = [{ node: root, depth: 1 }];
+  let maxDepth = -Infinity;
 
-var maxDepth = function(root) {
-  let depth = -1
-  function checkDepth(node, currDepth) {
-    if (node.left) {
-      checkDepth(node.left, currDepth+1)
-    }
-    if (node.right) {
-      checkDepth(node.right, currDepth+1)
-    }
+  while (queue.length > 0) {
+    const item = queue.pop();
+    if (!item) continue;
+    const { node, depth } = item;
+    maxDepth = Math.max(depth);
+
+    if (node?.right) queue.push({ node: node.right, depth: depth + 1 });
+    if (node?.left) queue.push({ node: node.left, depth: depth + 1 });
   }
+  return maxDepth;
+};
 
-
-}
-
-maxDepth(node3)
+const depth = maxDepth(root);
+console.log(depth);
+// maxDepth(node3);
