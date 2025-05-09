1dda94b99f42dabe326c8b751312daa9088e510b
Ahmad Muslih Zain <ahmadmuslih.zain@vivavis.com.tr>
2025-05-09 09:19:35 +0300

    fix 128

--- 128-longest-consecutive-sequence/app.py
+++ 128-longest-consecutive-sequence/app.py
@@ -3,7 +3,7 @@ class Solution:
         numSet = set(nums)
         longest = 0
 
-        for n in nums:
+        for n in numSet:
             if (n - 1) not in numSet:
                 length = 0
                 while (n + length) in numSet:
